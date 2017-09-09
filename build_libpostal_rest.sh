e8ab2e6e46bc8162f7a4b61d294c645d1f23a594
Sylwester Lachiewicz <slachiewicz@gmail.com>
2017-09-09 18:02:12 +0200

    Upgrade to Go 1.9

--- build_libpostal_rest.sh
+++ build_libpostal_rest.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-curl https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz > go1.8.1.linux-amd64.tar.gz
+curl https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz > go1.9.linux-amd64.tar.gz
 
-echo "a579ab19d5237e263254f1eac5352efcf1d70b9dacadb6d6bb12b0911ede8994  go1.8.1.linux-amd64.tar.gz" | sha256sum -c
+echo "d70eadefce8e160638a9a6db97f7192d8463069ab33138893ad3bf31b0650a79"  go1.9.linux-amd64.tar.gz" | sha256sum -c
 
-tar xzf go1.8.1.linux-amd64.tar.gz
+tar xzf go1.9.linux-amd64.tar.gz
 
 export GOROOT=/libpostal/go
 export GOPATH=/libpostal/workspace
